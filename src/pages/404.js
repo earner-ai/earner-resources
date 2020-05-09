324bcfc297c4dd2d21e87e9f38131b917e5b42ed
Kayla Gordon <kayla@Kayla-MacBook-Pro.local>
2020-05-09 22:08:45 +0300

    Update SEO import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
 import { Container } from "../components/Primitives"
 
 const NotFoundPage = () => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
 import {
   Container,
   ContentContainer,
